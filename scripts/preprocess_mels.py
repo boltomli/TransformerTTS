Changed normalization. Fix preprocessing, prediction.

scripts/preprocess_mels.py
@@ -29,7 +29,7 @@ config = configloader.config
 mel_dir = os.path.join(args.TARGET_DIR, 'mels')
 if not os.path.exists(mel_dir):
     os.makedirs(mel_dir)
-    
+
 phon_path = os.path.join(args.TARGET_DIR, 'phonemes.npy')
 if os.path.exists(phon_path):
     print("using cached phonemes")
@@ -62,8 +62,8 @@ else:
         np.save(phon_path, audio_data, allow_pickle=True)
 
 print('\nBuilding dataset and writing files')
-random.seed(42)
-random.shuffle(audio_data)
+np.random.seed(42)
+np.random.shuffle(audio_data)
 test_metafile = os.path.join(args.TARGET_DIR, 'test_metafile.txt')
 train_metafile = os.path.join(args.TARGET_DIR, 'train_metafile.txt')
 

utils/audio.py
@@ -7,18 +7,16 @@ def linear_to_mel(spectrogram, config):
         S=spectrogram, sr=config.sampling_rate, n_fft=config.n_fft, n_mels=config.mel_channels, fmin=config.f_min)
 
 
-'''
-def build_mel_basis():
-    return librosa.filters.mel(config.sampling_rate, config.n_fft, n_mels=config.mel_channels, fmin=config.fmin)
-'''
-
-
 def normalize(S, config):
-    return np.clip((S - config.min_level_db) / -config.min_level_db, 0, 1)
+    S = amp_to_db(S)
+    S = np.clip((S - config.min_level_db) / -config.min_level_db, 0, 1)
+    return (S * 2 * config.max_norm) - config.max_norm
 
 
 def denormalize(S, config):
-    return (np.clip(S, 0, 1) * -config.min_level_db) + config.min_level_db
+    S = (S + config.max_norm) / (2 * config.max_norm)
+    S = (np.clip(S, 0, 1) * -config.min_level_db) + config.min_level_db
+    return db_to_amp(S)
 
 
 def amp_to_db(x):
@@ -29,15 +27,9 @@ def db_to_amp(x):
     return np.power(10.0, x * 0.05)
 
 
-def spectrogram(y, config):
-    D = stft(y, config)
-    S = amp_to_db(np.abs(D)) - config.ref_level_db
-    return normalize(S, config)
-
-
 def melspectrogram(y, config):
     D = stft(y, config)
-    S = amp_to_db(linear_to_mel(np.abs(D), config))
+    S = linear_to_mel(np.abs(D), config)
     return normalize(S, config)
 
 
@@ -50,8 +42,7 @@ def stft(y, config):
 def reconstruct_waveform(mel, config, n_iter=32):
     """Uses Griffin-Lim phase reconstruction to convert from a normalized
     mel spectrogram back into a waveform."""
-    denormalized = denormalize(mel, config)
-    amp_mel = db_to_amp(denormalized)
+    amp_mel = denormalize(mel, config)
     S = librosa.feature.inverse.mel_to_stft(
         amp_mel, power=1, sr=config.sampling_rate,
         n_fft=config.n_fft, fmin=config.f_min)

utils/logging.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import tensorflow as tf
 
-from utils.audio import reconstruct_waveform, denormalize, db_to_amp
+from utils.audio import reconstruct_waveform, denormalize
 from utils.display import buffer_mel
 from preprocessing.utils import norm_tensor
 from utils.decorators import ignore_exception
@@ -82,8 +82,7 @@ class SummaryManager:
     
     @ignore_exception
     def display_mel(self, mel, config, tag='', sr=22050):
-        denormalized = denormalize(mel, config)
-        amp_mel = db_to_amp(denormalized)
+        amp_mel = denormalize(mel, config)
         img = tf.transpose(amp_mel)
         buf = buffer_mel(img, sr=sr)
         img_tf = tf.image.decode_png(buf.getvalue(), channels=3)